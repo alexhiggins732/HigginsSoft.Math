a67e85fb273ed9f1dfbf8cde46259bb748bec213
Alexander Higgins <alexander.higgins@Persante.com>
2025-05-08 18:49:46 -0400

    Split ECM

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -247,6 +247,55 @@ namespace TestRunner
 
         }
 
+        static void RunSplitEcm(string[] args)
+        {
+
+            var idx = args.ToList().IndexOf("n");
+            var n = args[idx + 1];
+            idx = args.ToList().IndexOf("threads");
+
+            int.TryParse(args[idx + 1], out int numThreads);
+            if (numThreads == 0)
+                numThreads = 1;
+
+
+            var curDir = Path.GetFullPath(".");
+            var baseDir = Path.Combine(curDir, "jobs");
+            var workStart = 30;
+            bool setMaxWork = false;
+            for (var i = 0; i < numThreads; i++)
+            {
+                var work = workStart + (5 * i);
+                var pretest = work + 2;
+                if (work>=65)
+                {
+                    if (setMaxWork)
+                        Console.WriteLine($"No more work after {65}");
+                    else
+                    {
+                        work = 64;
+                        pretest = 65;
+                    }
+                }    
+             
+                var cmd = $"\"{curDir}\\binaries\\yafu-x64.exe\"";
+                var cmdArgs = $"-work {work} -pretest {pretest} factor({n})";
+                var workingDirectory = Path.Combine(baseDir, $"job_{i}");
+
+                Directory.CreateDirectory(workingDirectory);
+                File.WriteAllLines(Path.Combine(workingDirectory, "yafu.ini"), ["threads=2"]);
+                var info = new ProcessStartInfo()
+                {
+                    FileName = cmd,
+                    Arguments = cmdArgs,
+                    WorkingDirectory = workingDirectory,
+                    UseShellExecute = true
+                };
+
+                Process.Start(info);
+            }
+        }
+
         private static void RunJobs()
         {
             /*
@@ -1007,7 +1056,7 @@ namespace TestRunner
 
                     var cmd = $"{exe} {algo}";
                     if (config.Curves.HasValue && config.Curves.Value > 0)
-                        cmd = $"{cmd}{(useGpu? "-gpucurves ": "-c ")}{config.Curves}";
+                        cmd = $"{cmd}{(useGpu ? "-gpucurves " : "-c ")}{config.Curves}";
                     cmd = $"{cmd} {config.B1}";
 
                     if (config.B2.HasValue && config.B2.Value > config.B1.Value)
@@ -1015,8 +1064,8 @@ namespace TestRunner
                     cmd = $"{cmd} < {batchFileName}";
                     var thisfactorWatch = Stopwatch.StartNew();
                     //Console.WriteLine($"Executing {cmd}");
-               
-                    var processResult = ProcessHelper.RunProcess(cmd, workingDirectory, WaitForExit:false);
+
+                    var processResult = ProcessHelper.RunProcess(cmd, workingDirectory, WaitForExit: false);
                     thisfactorWatch.Stop();
                     var results = processResult.Output.Split("Input number is").Skip(1).ToList();
                     //Console.WriteLine(processResult.Output);
